b57eada6aad22837ec553f322c87f90afab89ecd
tptww <tptww@outlook.com>
2024-10-25 00:16:33 +0800

    新增VarObjectArray类型

---
+++ UnityProject/Assets/UnityGameFramework/Scripts/Runtime/Variable/VarObjectArray.cs
@@ -0,0 +1,26 @@
+﻿using GameFramework;
+
+namespace UnityGameFramework.Runtime
+{
+    /// <summary>
+    /// System.Char 数组变量类。
+    /// </summary>
+    public sealed class VarObjectArray : Variable<object[]>
+    {
+        public VarObjectArray()
+        {
+        }
+
+        public static implicit operator VarObjectArray(object[] value)
+        {
+            VarObjectArray varValue = ReferencePool.Acquire<VarObjectArray>();
+            varValue.Value = value;
+            return varValue;
+        }
+
+        public static implicit operator object[](VarObjectArray value)
+        {
+            return value.Value;
+        }
+    }
+}
